4d264f8b195b6656ddae092b6bbc39966515fef9
Vegard Bjørsvik <vegardbjr@gmail.com>
2025-07-04 14:46:44 +0200

    Remove `output: 'standalone'` configuration from `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   poweredByHeader: false,
   // Configure any additional Next.js settings here
