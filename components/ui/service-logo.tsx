44909aa63ad8c9f82c1c9fcb6590bfb4e55c5504
Vegard Bjørsvik <vegardbjr@gmail.com>
2025-07-04 13:52:00 +0200

    Refactor `ServiceLogo` component to simplify class name handling and reduce redundant code.

--- components/ui/service-logo.tsx
+++ components/ui/service-logo.tsx
@@ -20,28 +20,18 @@ export default function ServiceLogo({
                                       height = 256,
                                       className
                                     }: ServiceLogoProps) {
-  if (!className) {
-    className = ""
-  }
-  if (!className.includes("h-")) {
-    className += " h-16"
-  }
-  if (!className.includes("w-")) {
-    className += " w-16"
-  }
-
-
+  const finalClassName = cn("h-16 w-16",className)
   return (
     <>
       {image && !fetching ? (
-        <div className={cn(className, "rounded overflow-hidden")}>
+        <div className={cn(finalClassName, "rounded overflow-hidden")}>
           <Image src={image} alt="Logo preview" width={width} height={height}/>
         </div>
       ) : fetching ? (
-        <Skeleton className={cn(className, "bg-muted rounded flex items-center justify-center")}>
+        <Skeleton className={cn(finalClassName, "bg-muted rounded flex items-center justify-center")}>
         </Skeleton>
       ) : (
-        <div className={cn(className, "bg-muted rounded flex items-center justify-center")}>
+        <div className={cn(finalClassName, "bg-muted rounded flex items-center justify-center")}>
           <span className="text-sm font-bold">{placeholder || '?'}</span>
         </div>
       )}
